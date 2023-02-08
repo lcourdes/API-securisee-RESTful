8da95f9a289662c1232ab0bc5a1e38554971b684
lcourdes <lea.courdes.murphy@gmail.com>
2023-02-08 11:17:56 +0100

    add user signup and login

---
+++ application/authentication/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from authentication.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ application/authentication/views.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from authentication.serializers import UserSerializer
+
+class CreateUserAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)#, serializer.data)
