6ec9937f9339fb893b61e3f6b4ba1d8e4e367a52
lcourdes <lea.courdes.murphy@gmail.com>
2023-05-26 16:18:36 +0200

    improve admin site

--- application/API/admin.py
+++ application/API/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from API.models import Project, Contributor
+from API.models import Project, Contributor, Issue, Comment
 
 admin.site.register(Project)
-admin.site.register(Contributor)
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ["project_id", "user_id"]
+
+
+admin.site.register(Contributor, ContributorAdmin)
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ["id", "project_id", "author_user_id"]
+
+
+admin.site.register(Issue, IssueAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "issue_id", "author_user_id"]
+
+
+admin.site.register(Comment, CommentAdmin)
